874629ef2ff2b8fd9875298dff03420cf72ea20f
aquaracer95 <aquaracer95@gmail.com>
2022-05-28 15:14:49 +0200

    Fixed login

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ const sessionConfig = {
     resave : false,
     saveUninitialized : true,
     cookie : {
-        secure: true,
         httpOnly : true,
         expires: Date.now() + 1000*60*60*24*7,
         maxAge: 1000*60*60*24*7
